ec041ef1f6635a15b27472b6526cf31fd6840f8f
swapnilroy1995 <41059087+swapnilroy1995@users.noreply.github.com>
2020-04-21 23:34:30 +0530

    Revert "Navigation"

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 import {AppRegistry} from 'react-native';
-import App from './src1/App'
-import countFunction from './src1/screens/Practise'
-import {expo} from 'app.json';
-const appName  =expo.name;
-
-// AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => countFunction);
-
